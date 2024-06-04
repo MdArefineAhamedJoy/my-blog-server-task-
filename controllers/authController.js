7e023664b766b1f58c3e7222eb1268f1e834a0fb
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2024-06-04 10:57:35 +0600

    add final commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -42,18 +42,6 @@ exports.login = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
-exports.verifyToken = (req, res) => {
-    const token = req.header('x-auth-token');
-    if (!token) return res.status(401).json({ error: 'No token, authorization denied' });
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
-        res.json({ message: 'Token is valid' });
-    } catch (error) {
-        res.status(400).json({ error: 'Token is not valid' });
-    }
-};
 
 exports.getAllUsers = async (req, res) => {
     try {
